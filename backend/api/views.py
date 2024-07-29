4d6171f1110a3863b5a74530fbbc1a98069548f1
maryam moshtoofar <maryammoshtoofar@gmail.com>
2024-07-29 10:59:23 +0330

    add create and delete to views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, PostSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Post
 
 # Create your views here.
+
+class PostListCreate(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Post.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author = self.request.user)
+        else:
+            print(serializer.errors)
+  
+class PostDelete(generics.DestroyAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Post.objects.filter(author=user)
+
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
