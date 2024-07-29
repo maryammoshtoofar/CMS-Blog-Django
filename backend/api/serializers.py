9503ac373a0832c9e25724cbb9cd79b358b64983
maryam moshtoofar <maryammoshtoofar@gmail.com>
2024-07-29 10:47:19 +0330

    add postSerializer to serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from .models import Post
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +11,9 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["id" , "title" , "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
