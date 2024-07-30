1f87a82c7deb7caaea869e528fc5cbd398216bcd
maryam moshtoofar <maryammoshtoofar@gmail.com>
2024-07-30 09:48:58 +0330

    add postgres config to settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
